f4fac35462ff8e172b7e7dca487f13c2ef6cbf48
ows3090 <ows3090@naver.com>
2023-05-08 18:19:37 +0900

    [chore] #1 WebView 테스트용 Naver URL 추가

---
+++ app/src/main/java/com/hobbyloop/manager/data/BaseUrl.kt
@@ -0,0 +1,11 @@
+package com.hobbyloop.manager.data
+
+/**
+ *   @Author Wonseok Oh
+ *   @Date 2023/05/08
+ *   @Company SKB
+ *   @Email ows3090@sk.com
+ */
+object Constants {
+    const val BASE_URL = "www.naver.com"
+}
