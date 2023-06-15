362707bc3dba4acda7f12be83ab93acc7c991377
ows3090 <ows3090@naver.com>
2023-06-15 18:06:03 +0900

    [feat] #7  네이버 로그인 SDK 초기화 코드 추가

--- app/src/main/java/com/hobbyloop/manager/HobbyLoopAdminApplication.kt
+++ app/src/main/java/com/hobbyloop/manager/HobbyLoopAdminApplication.kt
@@ -2,6 +2,7 @@ package com.hobbyloop.manager
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import com.navercorp.nid.NaverIdLoginSDK
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -17,5 +18,11 @@ class HobbyLoopAdminApplication : Application() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         KakaoSdk.init(this, BuildConfig.KAKAO_APP_KEY)
+        NaverIdLoginSDK.initialize(
+            this,
+            BuildConfig.NAVER_CLIENT_ID,
+            BuildConfig.NAVER_CLIENT_SECRET,
+            getString(R.string.naver_login_client_name)
+        )
     }
 }
